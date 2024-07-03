e4ecf4ea87538fcce361d11bceb58ba50cabb953
kopy-kat <kopp.konrad@gmail.com>
2024-07-03 09:30:48 +0200

    feat: add deployment script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import { Script } from "forge-std/Script.sol";
+import { MockValidator } from "src/mocks/MockValidator.sol";
+
+/**
+ * @title Deploy
+ * @author @kopy-kat
+ */
+contract DeployScript is Script {
+    function run() public {
+        bytes32 salt = bytes32(uint256(1));
+
+        vm.startBroadcast(vm.envUint("PK"));
+
+        // Deploy account and factory
+        new MockValidator{ salt: salt }();
+
+        vm.stopBroadcast();
+    }
+}
